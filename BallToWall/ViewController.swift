b86712348f34e75ecb62c33648aadf2cfc60affd
Toshiyuki Honda <t_honda@dreamarts.co.jp>
2019-01-14 12:19:09 +0900

    垂直面検出時に平面を配置、更新する処理を追加。

--- BallToWall/ViewController.swift
+++ BallToWall/ViewController.swift
@@ -58,6 +58,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
+    // 垂直面検出時に呼び出される
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor:
+        ARAnchor) {
+        // anchorがARPlaneAnchor以外なら何もしない
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        // ノードを作成
+        let planeNode = SCNNode()
+        // ジオメトリ(物体)として平面を指定
+        planeNode.geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x),
+                                height: CGFloat(planeAnchor.extent.z))
+        // 色を指定
+        planeNode.geometry?.materials.first?.diffuse.contents =
+            UIColor.cyan.withAlphaComponent(0.3)
+        // x軸方向に-90度回転(立てる)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+        
+        // 検出されたノードの子要素とする
+        node.addChildNode(planeNode)
+    }
+    
+    // 垂直面更新時に呼び出される
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for
+        anchor: ARAnchor) {
+        // anchorがARPlaneAnchor以外なら何もしない
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        // ジオメトリが平面でなければ何もしない
+        guard let planeNode = node.childNodes.first,
+            let planeGeometory = planeNode.geometry as? SCNPlane else
+        { return }
+        
+        // ジオメトリを更新
+        planeGeometory.width  = CGFloat(planeAnchor.extent.x)
+        planeGeometory.height = CGFloat(planeAnchor.extent.z)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0,
+                                                planeAnchor.center.z)
+    }
+    
 /*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
